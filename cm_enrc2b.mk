2b783b2efaf9d5d6700f3fd6190a1c6a49c0baea
maxwen <max.weninger@gmail.com>
2013-01-25 22:51:41 +0100

    changed bootanimation to blue

--- cm_enrc2b.mk
+++ cm_enrc2b.mk
@@ -130,8 +130,8 @@ PRODUCT_COPY_FILES += \
     device/htc/enrc2b/hostap/hostapd:system/bin/hostapd
 
 # boot animation
-#PRODUCT_COPY_FILES += \
-#    device/htc/enrc2b/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+    device/htc/enrc2b/bootanimation/bootanimation.zip:system/media/bootanimation.zip
     
 PRODUCT_PACKAGES += \
          hostapd_cli \
